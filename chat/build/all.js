37caab1d27a051264224b7588a6c654f1b291a32
j02004080 <j02004080@gmail.com>
2017-08-10 14:31:01 +0800

    'cloud'

--- chat/build/all.js
+++ chat/build/all.js
@@ -5,9 +5,9 @@ var SonicCoder = require('../lib/sonic-coder.js');
 
 var ALPHABET = '\n abcdefu0123456789\\';
 // Create an ultranet server.
-var sonicServer = new SonicServer({alphabet: ALPHABET, debug: true, freqMin: 10000, freqMax: 12000});
+var sonicServer = new SonicServer({alphabet: ALPHABET, debug: true, freqMin: 18000, freqMax: 19000});
 // Create an ultranet socket.
-var sonicSocket = new SonicSocket({alphabet: ALPHABET, freqMin: 10000, freqMax: 12000});
+var sonicSocket = new SonicSocket({alphabet: ALPHABET, freqMin: 18000, freqMax: 19000});
 
 
 var history = document.querySelector('#history');
